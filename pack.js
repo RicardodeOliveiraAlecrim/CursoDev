de4fd9028233d74882ae99a039739afd0d48ca62
RicardodeOliveiraAlecrim <ricardoalecrim2015@gmail.com>
2023-09-29 05:26:00 -0300

    Inicialização

---
+++ pack.js
@@ -0,0 +1,7 @@
+const packs = [
+
+    {name: 'frontEnd', value: 1200.00, begginer: true, src:'./assets/front.png' },
+    {name: 'fireBase', value: 1300.00, begginer: false, src:'./assets/firebase.png' },
+    {name: 'laravel', value: 1500.00, begginer: false, src:'./assets/lara.png' },
+    {name: 'rubyOnRails', value: 1800.00, begginer: false, src:'./assets/ruby.png' }
+]
---
+++ script.js
@@ -0,0 +1,39 @@
+const discount = document.querySelector("#discount")
+const integration = document.querySelector("#integration")
+const discountPrice = document.querySelector("#discountPrice")
+const only = document.querySelector("#only")
+
+
+
+function curses() {
+
+    discount.innerHTML = ' Damos um desconto de 10% para voçê começar agora.'
+
+    discountPrice.innerHTML = 'Desconto'
+
+    integration.innerHTML = ` Se junte aos 40 milhões de alunos que aprendem com o DevCursos
+    com garantia de 30 dias e um desconto de 10 %.Encontre o instrutor ideal para você.
+     Qualquer tema , nível ou idioma.Acesso vitalício. `
+
+
+    only.innerHTML = 'Tudo por apenas...'
+
+    const list = document.querySelector(".ul")
+
+    let newLi =''
+
+    list.innerHTML =
+
+    pack.array.forEach(product => {
+        `
+        <li>
+        <img src=${product.src}>
+        <p>${product.name}</p>
+        <p>R$ ${product.value}</p>
+        </li>
+
+        `  
+    });
+
+        
+}
